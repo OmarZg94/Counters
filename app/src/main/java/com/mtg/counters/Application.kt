89e7aa9036c71d087acceeb1d9da11a8b3eeb6ae
Omar Zuñiga <ozuniga@qsistemas.mx>
2021-03-06 12:35:18 -0600

    * Add Animations Between Fragments Flow * Manage Navigation From Welcome Screen To Main Screen

--- app/src/main/java/com/mtg/counters/Application.kt
+++ app/src/main/java/com/mtg/counters/Application.kt
@@ -1,6 +1,26 @@
 package com.mtg.counters
 
+import android.content.Context
+import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 
 class Application: MultiDexApplication() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        val TAG = "Counters Application"
+        private var instance: Application? = null
+
+        fun getContext(): Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        MultiDex.install(getContext())
+    }
 }
