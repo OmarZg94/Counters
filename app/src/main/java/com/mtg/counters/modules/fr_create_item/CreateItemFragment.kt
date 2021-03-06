ac8a4aa41093a6f03dfdd331d3fe1448df46402a
Omar Zuñiga <ozuniga@qsistemas.mx>
2021-03-06 16:58:51 -0600

    * Manage Blank Counter Name * Add Retrofit Dependencies

--- app/src/main/java/com/mtg/counters/modules/fr_create_item/CreateItemFragment.kt
+++ app/src/main/java/com/mtg/counters/modules/fr_create_item/CreateItemFragment.kt
@@ -1,41 +1,62 @@
 package com.mtg.counters.modules.fr_create_item
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.mtg.counters.R
+import com.mtg.counters.databinding.FragmentCreateItemBinding
+import com.mtg.counters.extensions.showKeyboard
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Use the [CreateItemFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CreateItemFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class CreateItemFragment : Fragment(), View.OnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentCreateItemBinding
+    private var name: String = ""
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_create_item, container, false)
+        initViews()
+        return binding.root
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_item, container, false)
+    private fun initViews() {
+        binding.edtNameItem.doOnTextChanged { text, _, _, _ -> name = text.toString().trim() }
+        binding.imgClose.setOnClickListener(this)
+        binding.txtSaveItem.setOnClickListener(this)
+        binding.txtSeeExamples.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            binding.imgClose.id -> findNavController().popBackStack()
+            binding.txtSeeExamples.id -> findNavController().navigate(R.id.present_examples)
+            binding.txtSaveItem.id -> {
+                if (name.isBlank()) {
+                    val alert = AlertDialog.Builder(activity)
+                    alert.setTitle(getString(R.string.wt_empty_name))
+                    alert.setMessage(getString(R.string.ws_empty_name))
+                    alert.setPositiveButton(getString(R.string.t_btn_ok), null)
+                    alert.show()
+                } else {
+                    binding.txtSaveItem.visibility = GONE
+                    binding.pgbCreateItem.visibility = VISIBLE
+                    binding.edtNameItem.isEnabled = false
+                }
+            }
+        }
     }
 
     companion object {
@@ -43,18 +64,13 @@ class CreateItemFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment CreateItemFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CreateItemFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        fun newInstance() =
+                CreateItemFragment().apply {
+                    arguments = Bundle().apply {
+                    }
                 }
-            }
     }
 }
