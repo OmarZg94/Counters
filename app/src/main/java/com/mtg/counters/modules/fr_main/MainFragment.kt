2c188d27499db87f8c0d67f2cbb8c2c043021079
Omar Zuñiga <ozuniga@qsistemas.mx>
2021-03-06 16:27:22 -0600

    * Create Examples Fragment * Connect Navigation Between Create Item Fragment And Examples Fragment * Manage Search Input In Main Toolbar

--- app/src/main/java/com/mtg/counters/modules/fr_main/MainFragment.kt
+++ app/src/main/java/com/mtg/counters/modules/fr_main/MainFragment.kt
@@ -1,26 +1,75 @@
 package com.mtg.counters.modules.fr_main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.*
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mtg.counters.Application
 import com.mtg.counters.R
 import com.mtg.counters.databinding.FragmentMainBinding
+import com.mtg.counters.extensions.hideKeyboard
+import com.mtg.counters.extensions.showKeyboard
 
 /**
  * A simple [Fragment] subclass.
  * Use the [MainFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), OnClickListener {
 
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        initViews()
         return binding.root
+    }
+
+    private fun initViews() {
+        binding.rcvCounters.layoutManager = LinearLayoutManager(Application.getContext(), RecyclerView.VERTICAL, false)
+        binding.edtSearch.doOnTextChanged { text, _, _, count ->
+            if (count == 0) {
+                binding.imgClose.visibility = INVISIBLE
+            } else {
+                binding.imgClose.visibility = VISIBLE
+            }
+        }
+        binding.imgSearchBar.setOnClickListener(this)
+        binding.imgBack.setOnClickListener(this)
+        binding.imgClose.setOnClickListener(this)
+        binding.btnAddCounter.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            binding.imgSearchBar.id -> {
+                binding.lytSearchBar.visibility = VISIBLE
+                binding.viewShadow.visibility = VISIBLE
+                binding.imgSearchBar.visibility = GONE
+                binding.edtSearch.isFocusableInTouchMode = true
+                binding.edtSearch.requestFocus()
+                binding.edtSearch.showKeyboard()
+            }
+            binding.imgClose.id -> {
+                binding.edtSearch.setText("")
+                binding.edtSearch.showKeyboard()
+            }
+            binding.imgBack.id -> {
+                binding.lytSearchBar.visibility = GONE
+                binding.viewShadow.visibility = GONE
+                binding.imgSearchBar.visibility = VISIBLE
+                binding.edtSearch.setText("")
+                binding.edtSearch.hideKeyboard()
+            }
+            binding.btnAddCounter.id -> findNavController().navigate(R.id.present_create_item)
+        }
     }
 
     companion object {
@@ -32,9 +81,9 @@ class MainFragment : Fragment() {
          */
         @JvmStatic
         fun newInstance() =
-            MainFragment().apply {
-                arguments = Bundle().apply {
+                MainFragment().apply {
+                    arguments = Bundle().apply {
+                    }
                 }
-            }
     }
 }
