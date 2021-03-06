ba1e3d1cf067492c1207f68bc1cec40c882ca336
Omar Zuñiga <ozuniga@qsistemas.mx>
2021-03-06 09:41:40 -0600

    * Add Navigation Component * Add Internet Permission In Manifest

--- app/src/main/java/com/mtg/counters/modules/MainActivity.kt
+++ app/src/main/java/com/mtg/counters/modules/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mtg.counters
+package com.mtg.counters.modules
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mtg.counters.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
